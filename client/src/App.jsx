a765871f193bdb0f9664e86a15258c61bcca961b
tonyekim <otoobongekim@gmail.com>
2024-02-28 16:16:08 +0100

    install react.js and cretate the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="underline text-red-400">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
