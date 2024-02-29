1c7cc3b0caaa85808381cba36054e15bf09b46ed
tonyekim <otoobongekim@gmail.com>
2024-02-29 12:22:04 +0100

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="underline text-red-400">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
