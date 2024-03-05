ab45171614afb094d481e152dc6ffabc0cad8803
tonyekim <otoobongekim@gmail.com>
2024-03-05 10:46:51 +0100

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -16,7 +17,13 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000, () => {
